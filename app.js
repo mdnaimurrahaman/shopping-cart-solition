22a0f0877eb3e391d2e1c1f189096ad1ffad66a6
mdnaimurrahaman <mdnaimurrahaman202@gmail.com>
2022-02-15 08:51:07 +0600

    calculate pricing

--- app.js
+++ app.js
@@ -1,29 +1,99 @@
-function updateCaseNumber(isIncreasing){
-    let caseInput = document.getElementById('case-number');
-    const caseNumber = caseInput.value ;
-    if(isIncreasing){
-        caseInput.value = parseInt (caseNumber) + 1 ;
-    }
-    else if(caseNumber>0){
-        caseInput.value = parseInt (caseNumber) - 1;
-    }
-    
+function updateProductNumber(product, price, isIncreasing) {
+  let productInput = document.getElementById( product+"-number");
+  let productNumber = productInput.value;
+  if (isIncreasing) {
+    productNumber = parseInt(productNumber) + 1;
+  } else if (productNumber > 0) {
+    productNumber = parseInt(productNumber) - 1;
+  }
+  productInput.value = productNumber;
+
+
+//--------------- update total case price------------------//
+let productTotal = document.getElementById(product +"-total");
+productTotal.innerText = productNumber * price;
+
+
+//------------- calculateTotal-----------------------//
+calculateTotal ();
 }
 
-// Case plus button clicked function
-function casePlus(){
-    updateCaseNumber(true);
-    // let caseInput = document.getElementById('case-number');
-    // const caseNumber = caseInput.value ;
-    // caseInput.value = parseInt (caseNumber) + 1 ;
-   
-}
-// Case minus button clicked function
-function caseMinus(){
-    updateCaseNumber(false);
-    // let caseInput = document.getElementById('case-number');
-    // const caseNumber = caseInput.value ;
-    
-    
 
+function getInputValue (product) {
+  const productInput = document.getElementById( product +'-number');
+  const productNumber =parseInt( productInput.value);
+  return productNumber ;
 }
+
+//---------------------- calculate total------------------//
+function calculateTotal (){
+ const phoneTotal = getInputValue('phone')* 1219 ;
+ const caseTotal = getInputValue ('case') * 59 ;
+ const subTotal = phoneTotal + caseTotal ;
+ const 
+}
+
+
+
+//--------- Phone increased decreased events---------------//
+function phonePlus(){
+  updateProductNumber('phone',1219, true);
+}
+function phoneMinus(){
+  updateProductNumber('phone',1219, false);
+}
+//---------- Case plus button clicked function------------//
+function casePlus() {
+  updateProductNumber('case',59, true);
+  // let caseInput = document.getElementById('case-number');
+  // const caseNumber = caseInput.value ;
+  // caseInput.value = parseInt (caseNumber) + 1 ;
+}
+//---------- Case minus button clicked function----------//
+function caseMinus() {
+  updateProductNumber('case',59,false);
+  // let caseInput = document.getElementById('case-number');
+  // const caseNumber = caseInput.value ;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* //--------- Phone increased discreased events---------------//
+function phonePlus(){
+  let phoneInput = document.getElementById('phone-number');
+  let phoneNumber = phoneInput.value;
+  phoneInput.value = parseInt(phoneNumber)+1;
+}
+//---------- Case minus button clicked function----------//
+function phoneMinus(){
+  let phoneInput = document.getElementById ('phone-number');
+  let phoneNumber = phoneInput.value ;
+  if(phoneNumber>0){
+    phoneInput.value = parseInt (phoneNumber)-1 ;
+  }
+}
+ */
---
+++ math.js
@@ -0,0 +1,9 @@
+
+// What will be the total?
+
+const product = 5; 
+const price = "7"; 
+const subTotal = product * price; 
+const tax = subTotal/10; 
+const total = subTotal + tax; 
+console.log(total);
